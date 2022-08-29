de07b4e00a18859e2285d2d9e465ca6da20fe1b2
Gina <gjuarez@sw1848.com>
2022-08-29 14:56:09 -0500

    first HW

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,10 +3,29 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
-}
-
-function DecimalABinario(num) {
-  // tu codigo aca
+  return num.split("").reverse().reduce((acc,cur, idx)=>{
+    return(cur==='1')?acc+Math.pow(2, idx):acc;
+   },0)
+   }
+  
+  
+  function DecimalABinario(num) {
+    // tu codigo aca
+  
+    var bin=[];
+  
+  for (var i=0; i<100 ; i++){
+  
+    bin.unshift(num%2);
+    num=Math.floor(num/2);
+  
+    if (num===1){
+      bin.unshift(num%2);
+      i=100;
+    }
+    }
+  
+    return bin.join('');
 
 }
 
